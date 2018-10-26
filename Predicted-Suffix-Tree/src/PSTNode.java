dc12dcbda143a6466e0595260d82a9d700cfc706
Eric Miao <hongyum@smu.edu>
2018-10-26 15:33:41 -0500

    add Unit test 3-b

--- Predicted-Suffix-Tree/src/PSTNode.java
+++ Predicted-Suffix-Tree/src/PSTNode.java
@@ -1,17 +1,14 @@
 import java.util.*;
 public class PSTNode<E> {
 
-	private ArrayList<PSTNode<E>> children = new ArrayList<PSTNode<E>>();
-	private ArrayList<E> word;
-	private ArrayList <ArrayList<E>> allSuffix = new ArrayList<ArrayList<E>>();
-	private int length;
-	private int index = 0;
+	private ArrayList<PSTNode<E>> children = new ArrayList<PSTNode<E>>();	//The children of the node
+	private ArrayList<E> word; // The value of node
+	private int length;//order of the PST tree
+	private int index = 0;	//This is the index of next node for isSuffix() and isFound() ----> it is useless at this point but i will combine it into my functions next week
 
-	PSTNode() 
-	{
-
-	}
-
+	//Constructors
+	PSTNode() {}
+	
 	PSTNode(int order) 
 	{
 		length = order;
@@ -22,76 +19,48 @@ public class PSTNode<E> {
 		word = input;
 	}
 
+	//Getter for getting the value of the node
 	ArrayList<E> getWord(int index)
 	{
 		return children.get(index).word;
 	}
 
+	//Getter for getting a specific node from the children arrayList
 	PSTNode<E> getNode(int index)
 	{
 		return children.get(index);
 	}
 
+	//Mainly for parsing files, this function will take the entie input data set and add each element from size of 1-length to the tree
 	void addToTree(ArrayList<E> input)	//addNode function
 	{
+		//Get the word from size of 1 to size of length
 		for(int i = 1; i <= length; i++)
 		{
+			//Get the word from first index to the last-i index
 			for(int j = 0; j < input.size()-i; j++)
 			{
-				//Loop through the data array and get each element from size of 0 to size of length
+				//create the temp arraylist to store the element we just get
 				ArrayList<E> temp = new ArrayList<E>(input.subList(j, j+i));
+				//using the temp to create a new temp node
 				PSTNode<E> newNode = new PSTNode(temp);
 
 				//Following function create the level 1 nodes: abrdc
 				if(newNode.word.size() == 1 && isLevel1Node(newNode) == true)
 				{
-					children.add(newNode); //If size == 1 (abcdr). Add to the children array
+					children.add(newNode); //If size == 1 (abcdr). Add to the this.children array
 				}
-				//				else if(newNode.word.size() > 1 && isSuffix(newNode.word) == true)
+			
+				//if the size > 1, we test if it isSuffix and add to the tree
 				else if(newNode.word.size() > 1)
 				{
 					isSuffix(newNode.word, this, 1);
-					//isFound(this,newNode.word,0,1); //Final step, add to the node
 				}
 			}
 		}
 	}
-
-//	boolean isFound(PSTNode node, ArrayList<E> val, int index, int loop)
-//	{	
-//		ArrayList<E> temp = new ArrayList<E>(val.subList(val.size()-loop, val.size()));
-//		for(int i = 0; i < node.children.size(); i++)
-//		{
-//			if(node.getNode(i).word.equals(temp))
-//			{
-//				index = i;
-//			}
-//		}
-//		while(loop < val.size()-1)
-//		{
-//NOTE: problem is here
-			//System.out.println("Index is "+ index +" val is "+val+" temp is "+temp);
-//			System.out.println("inner loop 1");
-//			PSTNode<E> tempNode = node.getNode(index);
-//			System.out.println("temp size = "+tempNode.children.size());
-//			isFound(tempNode,val,index,loop);
-			
-//--------------------------------------------------------------------------
-//			PSTNode<E> temp1 = node.getNode(index);
-//			node.children.remove(index);
-//			temp1.children.add(new PSTNode(val));
-//			node.children.add(temp1);
-//			loop++;
-//		}
-//		//After going through the loops
-//		System.out.println("val 2 = "+val + " node 2 = "+node.word);
-//		PSTNode<E> temp1 = node.getNode(index);
-//		node.children.remove(index);
-//		temp1.children.add(new PSTNode(val));
-//		node.children.add(temp1);
-//		return false;
-//	}
-
+	
+	//ifFound check if the input node value already exist in the tree, if not, we add it to the branch.
 	boolean ifFound(PSTNode node, int index, ArrayList<E> val)
 	{
 		
@@ -99,18 +68,19 @@ public class PSTNode<E> {
 		
 		for(int i = 0; i < newNode.children.size(); i++)
 		{
+			//If the size of input node.value is different than the node.value already in the target note's children arrayList
 			if(newNode.getWord(i).size() != val.size())
 			{
 				return true;
 			}
 			
-			//System.out.println("children word = " + newNode.getWord(i) + " and the value is = " + val);
+			//If we find a same word from the children arraylist, we return true
 			if(newNode.getWord(i).equals(val))
 			{
 				return true;
 			}
 		}
-		
+		//After node pass all those condition showing above, we move on.
 		addNote(newNode,val);
 		return false;
 	}
@@ -130,28 +100,38 @@ public class PSTNode<E> {
 		}
 	
 	
-	
+	//Add node to the branch
 	void addNote(PSTNode node, ArrayList<E> val)
 	{
 		PSTNode<E> newChild = new PSTNode(val);
 		node.children.add(newChild);
 	}
 	
-	void print(int time)	//Print function for testing
+	//Print function for testing
+	void print(int time)	
 	{
-		//System.out.print("   ");
+		
 		for(int i = 0; i < children.size(); i++)
 		{
+			printSpace(children.get(i).word.size());
 			System.out.println("-->"+children.get(i).word);
 			PSTNode temp = children.get(i);
 			time ++;
 			temp.print(time);
 		}
 	}
+	
+	void printSpace(int times)
+	{
+		for(int i = 0; i < times; i ++)
+		{
+			System.out.print("   ");
+		}
+	}
 
 	
 
-
+	//This function check if the input arraylist is suffix
 	boolean isSuffix(ArrayList<E> input, PSTNode currentNode, int loop)
 	{
 		ArrayList<E> temp = new ArrayList<E>();
@@ -160,6 +140,7 @@ public class PSTNode<E> {
 			temp.add(input.get(i));
 		}
 		
+		//Recursive from size of 1 to size of m-1 (from root to desired brunch)
 		while(loop < input.size()-1)
 		{
 			for (int i = 0; i < currentNode.children.size(); i ++)
@@ -167,14 +148,15 @@ public class PSTNode<E> {
 				if(currentNode.getWord(i).equals(temp) == true)
 				{
 					index = i;
-					//System.out.println("IsSuffix(): found = "+currentNode.getWord(index) + " and input is "+input); 
 					PSTNode<E> newNode = currentNode.getNode(index);
 					loop ++;
+					
 					isSuffix(input,newNode,loop);
 				}
 			}
 		}
 		
+		//When we found the desired brunch, we use ifFound to see if the node fulfill with our requirement. 
 		for (int i = 0; i < currentNode.children.size(); i ++)
 		{
 			if(currentNode.getWord(i).equals(temp) == true)
@@ -183,7 +165,6 @@ public class PSTNode<E> {
 				return true;
 			}
 		}
-		//System.out.println("IsSuffix(): temp Not Found = "+ input);
 		return false;
 	}
 	
