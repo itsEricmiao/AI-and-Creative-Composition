dd5d0e80c6f43b4e92e740017b322ea3a154efb5
Eric Miao <hongyum@smu.edu>
2018-10-26 13:26:20 -0500

    abracadabra is working

--- Predicted-Suffix-Tree/src/PSTNode.java
+++ Predicted-Suffix-Tree/src/PSTNode.java
@@ -3,105 +3,191 @@ public class PSTNode<E> {
 
 	private ArrayList<PSTNode<E>> children = new ArrayList<PSTNode<E>>();
 	private ArrayList<E> word;
+	private ArrayList <ArrayList<E>> allSuffix = new ArrayList<ArrayList<E>>();
 	private int length;
 	private int index = 0;
-	
+
 	PSTNode() 
 	{
-        
-    }
-	
+
+	}
+
 	PSTNode(int order) 
 	{
-        length = order;
-    }
-	
+		length = order;
+	}
+
 	PSTNode(ArrayList<E> input) 
 	{
-        word = input;
-    }
+		word = input;
+	}
+
+	ArrayList<E> getWord(int index)
+	{
+		return children.get(index).word;
+	}
+
+	PSTNode<E> getNode(int index)
+	{
+		return children.get(index);
+	}
 
 	void addToTree(ArrayList<E> input)	//addNode function
 	{
-		for(int i = 0; i <= length; i++)
+		for(int i = 1; i <= length; i++)
 		{
 			for(int j = 0; j < input.size()-i; j++)
 			{
+				//Loop through the data array and get each element from size of 0 to size of length
 				ArrayList<E> temp = new ArrayList<E>(input.subList(j, j+i));
 				PSTNode<E> newNode = new PSTNode(temp);
-				System.out.println(temp);
-				
-				//Create the level 1 nodes: abrdc
+
+				//Following function create the level 1 nodes: abrdc
 				if(newNode.word.size() == 1 && isLevel1Node(newNode) == true)
 				{
 					children.add(newNode); //If size == 1 (abcdr). Add to the children array
 				}
-				else if(newNode.word.size() > 1 && isSuffix(newNode.word) == true)
+				//				else if(newNode.word.size() > 1 && isSuffix(newNode.word) == true)
+				else if(newNode.word.size() > 1)
 				{
-					addToNode(newNode); //Final step, add to the node
+					isSuffix(newNode.word, this, 1);
+					//isFound(this,newNode.word,0,1); //Final step, add to the node
 				}
 			}
 		}
 	}
 
-	void addToNode(PSTNode child)
+//	boolean isFound(PSTNode node, ArrayList<E> val, int index, int loop)
+//	{	
+//		ArrayList<E> temp = new ArrayList<E>(val.subList(val.size()-loop, val.size()));
+//		for(int i = 0; i < node.children.size(); i++)
+//		{
+//			if(node.getNode(i).word.equals(temp))
+//			{
+//				index = i;
+//			}
+//		}
+//		while(loop < val.size()-1)
+//		{
+//NOTE: problem is here
+			//System.out.println("Index is "+ index +" val is "+val+" temp is "+temp);
+//			System.out.println("inner loop 1");
+//			PSTNode<E> tempNode = node.getNode(index);
+//			System.out.println("temp size = "+tempNode.children.size());
+//			isFound(tempNode,val,index,loop);
+			
+//--------------------------------------------------------------------------
+//			PSTNode<E> temp1 = node.getNode(index);
+//			node.children.remove(index);
+//			temp1.children.add(new PSTNode(val));
+//			node.children.add(temp1);
+//			loop++;
+//		}
+//		//After going through the loops
+//		System.out.println("val 2 = "+val + " node 2 = "+node.word);
+//		PSTNode<E> temp1 = node.getNode(index);
+//		node.children.remove(index);
+//		temp1.children.add(new PSTNode(val));
+//		node.children.add(temp1);
+//		return false;
+//	}
+
+	boolean ifFound(PSTNode node, int index, ArrayList<E> val)
 	{
-		PSTNode temp = children.get(index);
-		children.remove(index);
-		if(isFound(child.word) == false)
+		
+		PSTNode newNode = node.getNode(index);
+		
+		for(int i = 0; i < newNode.children.size(); i++)
 		{
-			temp.children.add(child);
-			children.add(temp);
-		}
-	}
-	
-	void print()	//Print function for testing
-	{
-		for(int i = 0; i < children.size(); i++)
-		{
-			System.out.println("-->"+children.get(i).word);
-			for(int j = 0; j < children.get(i).children.size(); j++)
+			if(newNode.getWord(i).size() != val.size())
 			{
-				System.out.println("   -->"+children.get(i).children.get(j).word);
+				return true;
 			}
+			
+			System.out.println("children word = " + newNode.getWord(i) + " and the value is = " + val);
+			if(newNode.getWord(i).equals(val))
+			{
+				System.out.println("DO not add");
+				return true;
+			}
+			System.out.println();
 		}
+		
+		addNote(newNode,val);
+		return false;
 	}
 	
 	//This function is for level 1 of the PST tree (size of the node == 1)
-	boolean isLevel1Node(PSTNode level1Node)
-	{
-		for (int i = 0; i < children.size(); i ++)
+		boolean isLevel1Node(PSTNode level1Node)
 		{
-			if(children.get(i).word.equals(level1Node.word) == true)
+			for (int i = 0; i < children.size(); i ++)
 			{
-				return false;
+				if(children.get(i).word.equals(level1Node.word) == true)
+				{
+					
+					return false;
+				}
 			}
+			return true;
 		}
-		return true;
-	}
-
-	boolean isFound(ArrayList<E> val)
+	
+	
+	
+	void addNote(PSTNode node, ArrayList<E> val)
 	{
-		
-		return false;
+		PSTNode<E> newChild = new PSTNode(val);
+		node.children.add(newChild);
 	}
 	
-	boolean isSuffix(ArrayList<E> input)
+	void print(int time)	//Print function for testing
+	{
+		//System.out.print("   ");
+		for(int i = 0; i < children.size(); i++)
+		{
+			System.out.println("-->"+children.get(i).word);
+			PSTNode temp = children.get(i);
+			time ++;
+			temp.print(time);
+		}
+	}
+
+	
+
+
+	boolean isSuffix(ArrayList<E> input, PSTNode currentNode, int loop)
 	{
 		ArrayList<E> temp = new ArrayList<E>();
-		for(int i = input.size()-length+1; i < input.size(); i++)
+		for(int i = input.size()-loop; i < input.size(); i++)
 		{
 			temp.add(input.get(i));
 		}
-		for (int i = 0; i < children.size(); i ++)
+		
+		while(loop < input.size()-1)
 		{
-			if(children.get(i).word.equals(temp) == true)
+			for (int i = 0; i < currentNode.children.size(); i ++)
 			{
-				index = i;
+				if(currentNode.getWord(i).equals(temp) == true)
+				{
+					index = i;
+					//System.out.println("IsSuffix(): found = "+currentNode.getWord(index) + " and input is "+input); 
+					PSTNode<E> newNode = currentNode.getNode(index);
+					loop ++;
+					isSuffix(input,newNode,loop);
+				}
+			}
+		}
+		
+		for (int i = 0; i < currentNode.children.size(); i ++)
+		{
+			if(currentNode.getWord(i).equals(temp) == true)
+			{
+				ifFound(currentNode, i,input);
 				return true;
 			}
 		}
+		//System.out.println("IsSuffix(): temp Not Found = "+ input);
 		return false;
-		
 	}
+	
+	
 }
